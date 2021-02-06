18cb002f1597bf3b708dd89c963d89e26a658f05
Breens-Thomas <mbakabreens@gmail.com>
2021-02-06 21:34:01 +0300

    Add class for the creation of repository object during search

--- src/app/repositories.ts
+++ src/app/repositories.ts
@@ -1,4 +1,8 @@
 export class Repositories {
-    constructor(public repoName:string,public description: string) {
+    constructor(
+    public repoName:string,
+    public description: string,
+    public stars:number,
+    public watching:number) {
     }
 }
