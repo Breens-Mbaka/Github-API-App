08af1a38ec111dff2faa0005b1ea3b13259bfeac
Breens-Thomas <mbakabreens@gmail.com>
2021-02-07 07:34:29 +0300

    Make the the username and username-data accept data from github-display component,so as to display the data

--- src/app/username/username.component.ts
+++ src/app/username/username.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-username',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsernameComponent implements OnInit {
 
+  @Input() userProfile:any;
   constructor() { }
 
   ngOnInit(): void {
