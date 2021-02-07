d87e5a5d96e9c07f9e6251c10ce574766be6ade1
Breens-Thomas <mbakabreens@gmail.com>
2021-02-07 08:52:42 +0300

    Let the projects component display the projects by letting it accept data from the github display component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  @Input() userProjects!:any[];
   constructor() { }
 
   ngOnInit(): void {
